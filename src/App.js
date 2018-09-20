be8b3488fc5a7e6ea8d1445b3f63c367a90b801f
Robert Bissell <roblincolnbissell@gmail.com>
2018-09-20 16:27:52 -0500

    burger menu working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import './styles/burger-menu.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+import { scaleRotate as Menu } from 'react-burger-menu';
+
 class App extends Component {
+
+  showSettings (event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div id='master-container'>
-        <Header/>
-        <Main/>
+        <div id="outer-container">
+          <Menu pageWrapId={ "page-wrap-test"} outerContainerId={ "outer-container" }>
+            <a id="home" className="menu-item" href="/">Home</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+          </Menu>
+          <div id="page-wrap-test">
+            <Header/>
+            <Main/>
+          </div>
+        </div>
         <Footer/>
       </div>
     );
