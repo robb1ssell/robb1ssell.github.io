c4076eccc4a40dd7615e85a3a292a8b6b5c229e1
Robert Bissell <roblincolnbissell@gmail.com>
2018-09-21 13:05:52 -0500

    added typed js, adjust some style and color of menu and #main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Typer from './Typer';
+
 
 class Main extends Component {
+
+  
   render() {
     return (
       <div id='main'>
         <h2>FRONT END</h2>
         <h1>WEB DEVELOPER</h1>
         <hr className='divider'/>
+        <Typer
+          strings={['<h2>FRONT END</h2>', '<h1>WEB DEVELOPER</h1>']}
+        />
       </div>
     );
   }
