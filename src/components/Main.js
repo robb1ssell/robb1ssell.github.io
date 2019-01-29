e4ceb3dec5d426689b8c1c0808c227380ae61cb6
Robert Bissell <roblincolnbissell@gmail.com>
2019-01-29 09:12:35 -0600

    Separated intro text into own component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Typer from './Typer';
+import IntroTextEvent from './IntroTextEvent';
 
 
 class Main extends Component {
@@ -8,13 +8,7 @@ class Main extends Component {
   render() {
     return (
       <div id='main'>
-        <div id='intro-text' className='intro fadeOut'>
-          <Typer
-            strings={[
-              '$ npm run build^400\n `  npm-run-all build-css build-js` ^1000\n `  node-sass-chokidar src/ -o src/` ^1000\n `  Creating an optimized production build...` ^2000\n `  Compiled Successfully. Again... \n``$ `^1500serve -s build ^400\n`   bash: serve: command not found`^1000\n `  ...`^1500\n   kidding...'
-            ]}
-          />
-        </div>
+        <IntroTextEvent/>
       </div>
     );
   }
