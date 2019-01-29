035479ecc169c386f14f2f26618953544b5af5fd
Robert Bissell <roblincolnbissell@gmail.com>
2019-01-29 14:48:27 -0600

    removed burger menu

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import './styles/burger-menu.css';
-
-import { scaleRotate as Menu } from 'react-burger-menu';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 class App extends Component {
 
-  showSettings (event) {
-    event.preventDefault();
-  }
-
   render() {
     return (
       <div id='master-container'>
-        <div id="outer-container">
-          <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-          </Menu>
-
-
-          <div className="container">
-            <div id="page-wrap">
-              <Main/>
-            </div>
+        <div className="container">
+          <div id="page-wrap">
+            <Main/>
           </div>
-          <Footer/>
         </div>
+        <Footer/>
       </div>
     );
   }
