af999db05a81aea31809138d764c87d0ef97e93b
Robert Bissell <roblincolnbissell@gmail.com>
2019-01-29 09:07:12 -0600

    added timed fade out for intro text event

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,16 +8,13 @@ class Main extends Component {
   render() {
     return (
       <div id='main'>
-      {/*
-        <h2>FRONT END</h2>
-        <h1>WEB DEVELOPER</h1>
-        <hr className='divider'/>
-      */}
-        <Typer
-          strings={[
-            '$ npm run build^400\n `  npm-run-all build-css build-js` ^1000\n `  node-sass-chokidar src/ -o src/` ^1000\n `  Creating an optimized production build...` ^2000\n `  Compiled Successfully. Again... \n``$ `^1500serve -s build ^400\n`   bash: serve: command not found`^1000\n `  ...`^1500\n   kidding...'
-          ]}
-        />
+        <div id='intro-text' className='intro fadeOut'>
+          <Typer
+            strings={[
+              '$ npm run build^400\n `  npm-run-all build-css build-js` ^1000\n `  node-sass-chokidar src/ -o src/` ^1000\n `  Creating an optimized production build...` ^2000\n `  Compiled Successfully. Again... \n``$ `^1500serve -s build ^400\n`   bash: serve: command not found`^1000\n `  ...`^1500\n   kidding...'
+            ]}
+          />
+        </div>
       </div>
     );
   }
