9dbde20b8d92d518def6859162d9e0d373532cb6
Robert Bissell <roblincolnbissell@gmail.com>
2019-01-31 01:26:56 -0600

    fixed class to className on top social fa tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,9 @@ class Header extends Component {
           <h2>Sports Buff <span className="bar">|</span> Gaming Nerd</h2>
         </div>
         <div id="top-socials">
-          <i class="fa fa-linkedin" aria-hidden="true"></i>
-          <i class="fa fa-github" aria-hidden="true"></i>
-          <i class="fa fa-envelope-o" aria-hidden="true"></i>
+          <i className="fa fa-linkedin" aria-hidden="true"></i>
+          <i className="fa fa-github" aria-hidden="true"></i>
+          <i className="fa fa-envelope-o" aria-hidden="true"></i>
         </div>
       </div>
     );
