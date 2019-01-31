81426dbed21a56a2561bededb69e057c835c33e8
Rob Bissell <roblincolnbissell@gmail.com>
2019-01-30 20:47:36 -0600

    added cv button, not functional yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div id='header' className='initialFadeIn fadeIn'>
+        <button id="download-cv-button">DOWNLOAD CV</button>
         <img id='headpic' src="https://media.licdn.com/dms/image/C4E03AQHqVMEDXDkBoQ/profile-displayphoto-shrink_200_200/0?e=1554336000&v=beta&t=eIM9QwZCArfF5CDTD-t0UzNMrWmZRb7m0NFnTwOgk_0" 
           className='center-img' alt="Rob Headshot"/>
         <h1>Robert Bissell</h1>
