ed1367b0c486fc6ae2c4554d4a2d943892998045
Rob Bissell <roblincolnbissell@gmail.com>
2019-01-31 21:13:43 -0600

    added initial skill chart using react chart js 2

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,24 @@
 import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+const data = {
+  labels: [
+    'Label'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+      '#FF6384',
+		  '#36A2EB',
+		  '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ]
+  }]
+};
 
 class Skills extends Component {
   render() {
@@ -7,6 +27,9 @@ class Skills extends Component {
         <section id="skills" className='page-content'>
           <div className="section-header">
             <h1>Skills</h1>
+          </div>
+          <div className="col-sm-6">
+            <Doughnut data={data}/>
           </div>
         </section>
       </div>
