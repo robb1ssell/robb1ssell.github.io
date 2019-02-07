40eab88eae0cb0671ad6d67f4487153be970baf3
Rob Bissell <roblincolnbissell@gmail.com>
2019-02-06 21:13:08 -0600

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
-
-Please use npm install and npm start to view project after downloading.
-
-This is my personal resume website, built from the ground up by myself using React.
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { env } from './data/messageConfig';
-
-ReactDOM.render(<App env={env} />, document.getElementById('root'));
-registerServiceWorker();
